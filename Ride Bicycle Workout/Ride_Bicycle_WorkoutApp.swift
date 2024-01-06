ed7152153c2cf6add4685c4486d16a4e70c7864f
Harshit Agarwal <harshitagarwal46446@icloud.com>
2024-01-06 21:16:11 +0530

    Initial Commit

---
+++ Ride Bicycle Workout/Ride_Bicycle_WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ride_Bicycle_WorkoutApp.swift
+//  Ride Bicycle Workout
+//
+//  Created by Harshit Agarwal on 06/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Ride_Bicycle_WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
